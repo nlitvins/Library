93f79a657a6c18765aaccaa9e39e362775992dd9
nlitvins <barbos3100@gmail.com>
2024-12-03 12:40:29 +0200

    List added

--- Library.java
+++ Library.java
@@ -1,62 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.UUID;
 
 public class Library {
-    int[] id = new int[]{1231, 1232, 1233, 1234};
-    String[] title = new String[]{"Fiona", "Shrek", "Lord Farquaad", "Prince Charming"};
-    String[] author = new String[]{"Mike", "John", "Jack", "Ivan"};
-    boolean[] isBorrowed = new boolean[]{true, true, true, false};
+
+    private final List<Book> books = new ArrayList<>();
+
+    public Library() {
+        books.add(new Book(1231, "Fiona", "Mike", true));
+        books.add(new Book(1232, "Shrek", "John", true));
+        books.add(new Book(1233, "Lord Farquaad", "Jack", true));
+        books.add(new Book(1234, "Prince Charming", "Ivan", false));
+    }
 
     public void addBook() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter book ID: ");
-        int newId = Integer.parseInt(scanner.nextLine());
-        scanner.nextLine();
+        final int newId = Integer.parseInt(scanner.nextLine());
 
         System.out.print("Enter book title: ");
-        String newTitle = scanner.nextLine();
-        scanner.nextLine();
+        final String newTitle = scanner.nextLine();
 
         System.out.print("Enter book author: ");
-        String newAuthor = scanner.nextLine();
-        scanner.nextLine();
+        final String newAuthor = scanner.nextLine();
 
 
-        this.id = expandArray(this.id, newId);
-        title = expandArray(title, newTitle);
-        author = expandArray(author,  newAuthor);
-        isBorrowed = expandArray(isBorrowed, false);
-
-
+        books.add(new Book(newId, newTitle, newAuthor, false));
         System.out.println("Book added successfully! ");
     }
 
-    public void displayBooks(){
+    public void displayBooks() {
         System.out.println("Library books: ");
-        for (int i = 0; i < id.length; i++) {
-            System.out.println("ID: " + id[i] + ", Title: " + title[i] + ", Author: " + author[i] + ", Borrowed: " + isBorrowed[i]);
+        for (Book book : books) {
+            System.out.println(book);
         }
-    }
-
-    private int[] expandArray(int[] original, int newElement){
-        int[] newArray = new int[original.length + 1];
-        System.arraycopy(original, 0, newArray, 0, original.length);
-        newArray[original.length] = newElement;
-        return newArray;
-    }
-
-    private String[] expandArray(String[] original, String newElement){
-        String[] newArray = new String[original.length + 1];
-        System.arraycopy(original, 0, newArray, 0, original.length);
-        newArray[original.length] = newElement;
-        return newArray;
-    }
-
-    private boolean[] expandArray(boolean[] original, boolean newElement){
-     boolean[] newArray = new boolean[original.length +1];
-     System.arraycopy(original, 0, newArray, 0, original.length);
-     newArray[original.length] = newElement;
-     return newArray;
     }
 }
