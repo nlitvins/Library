7c45e86754b189a12fdefb0d6c8437e4085afe14
nlitvins <barbos3100@gmail.com>
2024-12-09 10:51:26 +0200

    Main updated

--- Library.java
+++ Library.java
@@ -1,33 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Library {
 
-    private final List<Book> books = new ArrayList<>();
+    public static final List<Book> books = new ArrayList<>();
 
     public Library() {
         books.add(new Book(1231, "Fiona", "Mike", true));
         books.add(new Book(1232, "Shrek", "John", true));
         books.add(new Book(1233, "Lord Farquaad", "Jack", true));
         books.add(new Book(1234, "Prince Charming", "Ivan", false));
-    }
-
-    public void addBook() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter book ID: ");
-        final int newId = Integer.parseInt(scanner.nextLine());
-
-        System.out.print("Enter book title: ");
-        final String newTitle = scanner.nextLine();
-
-        System.out.print("Enter book author: ");
-        final String newAuthor = scanner.nextLine();
-
-
-        books.add(new Book(newId, newTitle, newAuthor, false));
-        System.out.println("Book added successfully! ");
     }
 
     public void displayBooks() {
--- Main.java
+++ Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Main{
+
+    public static void addBook() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter book ID: ");
+        final int newId = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Enter book title: ");
+        final String newTitle = scanner.nextLine();
+
+        System.out.print("Enter book author: ");
+        final String newAuthor = scanner.nextLine();
+
+        Library.books.add(new Book(newId, newTitle, newAuthor, false));
+        System.out.println("Book added successfully! ");
+    }
+
     public static void main(String[] args){
         Library library = new Library();
         library.displayBooks();
-        library.addBook();
+        addBook();
         library.displayBooks();
         library.findAll();
     }
