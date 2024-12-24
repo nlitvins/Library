79e3697c44efa160368773d7dad54570026abe81
nlitvins <75936559+nlitvins@users.noreply.github.com>
2024-12-24 13:12:53 +0200

    LP-3: Library (repository): add update functionality (#7)

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -18,7 +18,7 @@ class LibraryTest {
     }
 
     @Test
-    void addBook() {
+    void whenAddBookAddNewBookAndReturn() {
         assertNull(sut.findById(1237));
         Book book = new Book(1237, "Idiot", "Chehov", false);
 
@@ -38,7 +38,7 @@ class LibraryTest {
     }
 
     @Test
-    void findById() {
+    void whenFindByIdReturnBook() {
         Book result = sut.findById(1233);
 
         Book expected = new Book(1233, "Lord Farquaad", "Jack", true);
@@ -46,7 +46,7 @@ class LibraryTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllReturnList() {
         List<Book> result = sut.findAll();
 
         List<Book> expected = new ArrayList<>();
@@ -58,12 +58,27 @@ class LibraryTest {
     }
 
     @Test
-    void deleteById() {
+    void whenDeleteByIdBookDeleted() {
         assertNotNull(sut.findById(1231));
 
         sut.deleteById(1231);
 
         Book result = sut.findById(1231);
         assertNull(result);
+    }
+
+    @Test
+    void whenUpdateBookStatusExists() {
+        Book result = sut.updateBookStatus(1231);
+
+        Book expected = new Book(1231, "Fiona", "Mike", false);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void whenUpdateStatusBookDontExistsThenThrowException() {
+        RuntimeException thrown = assertThrows(RuntimeException.class, () -> sut.updateBookStatus(1255));
+
+        assertEquals("Book don't exist", thrown.getMessage());
     }
 }
