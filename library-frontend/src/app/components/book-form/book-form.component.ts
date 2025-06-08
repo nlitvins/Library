7e19b4e23496f30d77d402ade6aeb06650cb4514
nlitvins <barbos3100@gmail.com>
2025-06-08 19:40:33 +0300

    LP-23: Add fields and genre to Book

--- library-frontend/src/app/components/book-form/book-form.component.ts
+++ library-frontend/src/app/components/book-form/book-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Book, BookService} from '../../services/book.service';
+import {Book, BookGenre, BookService, BookStatus, BookType} from '../../services/book.service';
 import {Router} from '@angular/router';
 
 @Component({
@@ -8,9 +8,23 @@ import {Router} from '@angular/router';
   styleUrls: ['./book-form.component.scss']
 })
 export class BookFormComponent implements OnInit {
-  book: Book = {title: '', author: '', quantity: 1};
+  book: Book = {
+    title: '',
+    author: '',
+    quantity: 1,
+    creationYear: new Date().toISOString().split('T')[0],
+    status: BookStatus.AVAILABLE,
+    genre: BookGenre.CLASSIC,
+    pages: 0,
+    edition: '',
+    releaseDate: new Date().toISOString().split('T')[0],
+    type: BookType.BOOK
+  };
   loading = false;
   error: string | null = null;
+  bookStatuses = Object.values(BookStatus);
+  bookGenres = Object.values(BookGenre);
+  bookTypes = Object.values(BookType);
 
   constructor(private bookService: BookService, private router: Router) {
   }
