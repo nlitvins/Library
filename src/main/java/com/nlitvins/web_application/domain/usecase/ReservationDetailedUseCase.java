bf087a21650a599628624fa76826bffc9dcf9808
nlitvins <barbos3100@gmail.com>
2025-06-13 16:13:00 +0300

    LP-28: Add Reservation Details for My Reservations and Users list

--- src/main/java/com/nlitvins/web_application/domain/usecase/ReservationDetailedUseCase.java
+++ src/main/java/com/nlitvins/web_application/domain/usecase/ReservationDetailedUseCase.java
@@ -1,7 +1,9 @@
 package com.nlitvins.web_application.domain.usecase;
 
 import com.nlitvins.web_application.domain.model.ReservationDetailed;
+import com.nlitvins.web_application.domain.repository.JwtRepository;
 import com.nlitvins.web_application.domain.repository.ReservationDetailedRepository;
+import io.jsonwebtoken.lang.Collections;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,13 +12,23 @@ import java.util.List;
 public class ReservationDetailedUseCase {
 
     private final ReservationDetailedRepository reservationDetailedRepository;
+    private final JwtRepository jwtRepository;
 
-    public ReservationDetailedUseCase(ReservationDetailedRepository reservationDetailedRepository) {
+    public ReservationDetailedUseCase(ReservationDetailedRepository reservationDetailedRepository, JwtRepository jwtRepository) {
         this.reservationDetailedRepository = reservationDetailedRepository;
+        this.jwtRepository = jwtRepository;
     }
 
     public List<ReservationDetailed> findAll() {
         return reservationDetailedRepository.findAll();
+    }
+
+    public List<ReservationDetailed> getReservationsByToken(String token) {
+        Integer userId = jwtRepository.getUserId(token);
+        if (userId == null) {
+            return Collections.emptyList();
+        }
+        return findByUserId(userId);
     }
 
     public List<ReservationDetailed> findByUserId(int userId) {
