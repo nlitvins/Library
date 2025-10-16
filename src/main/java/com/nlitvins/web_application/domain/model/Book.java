7d2538a08ffbc6a925f5ba548639f2d61827534f
dlitvins <dmitrijs.litvins@gmail.com>
2025-10-16 08:40:09 +0300

    .

--- src/main/java/com/nlitvins/web_application/domain/model/Book.java
+++ src/main/java/com/nlitvins/web_application/domain/model/Book.java
@@ -1,22 +1,23 @@
 package com.nlitvins.web_application.domain.model;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Setter
 @Getter
+@ToString
 @Builder(toBuilder = true)
+@EqualsAndHashCode
 public class Book {
 
     private int id;
     private String title;
     private String author;
-    //TODO
-    private boolean isBorrowed;
     private int quantity;
     private LocalDate creationYear;
     private BookStatus status;
@@ -25,21 +26,4 @@ public class Book {
     private String edition;
     private LocalDate releaseDate;
     private BookType type;
-
-    @Override
-    public String toString() {
-        return "Book: " +
-                "id: " + id +
-                ", " + title +
-                ", " + author +
-                ", " + isBorrowed +
-                ' ';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Book book)) return false;
-        return id == book.id && isBorrowed == book.isBorrowed && Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
 }
